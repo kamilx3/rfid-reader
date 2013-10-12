aab512a5daa9963825e99883ebda1c3e28cdd095
kamilx3 <kamilx3@gmail.com>
2013-10-12 11:11:37 +0200

    Fixed rfid_reader.ino not to hang after reading first tag.

--- rfid_reader.ino
+++ rfid_reader.ino
@@ -1,6 +1,6 @@
 #include <SoftwareSerial.h>
-#define rxPin 3
-#define txPin 2
+#define rxPin 10
+#define txPin 11
 SoftwareSerial rfserial = SoftwareSerial(rxPin, txPin);
 
 byte SeekCard[] = {0xFF,0x00,0x01,0x82,0x83};
@@ -9,54 +9,50 @@ byte Prim_Card[] = {0xFF,0x00,0x06,0x82,0x02,0xCD,0xD0,0xFC,0x64,0x87};
 void setup(){
     Serial.begin(9600);
     rfserial.begin(19200);
-  }
+}
 
 void loop(){
-  SeekTag();
-  delay(2000);
+    SeekTag();
+    delay(1000);
 }
 
-void printbad()
-{
-Serial.println("Error Reading Error");
+void printbad(){
+    Serial.println("Error Reading Error");
 }
 
-void SeekTag()
-{
-
+void SeekTag(){
     //an array for the RFID response acknowledging command
     byte ModResponse[6];
     //checksum reference
     byte modrespchecksum = 0xD0;
     //checkum checker
     byte response_checksum_math;
-//an array for the RFID card data that has been found
+    //an array for the RFID card data that has been found
     byte CardResponse[10];
     //checksum variable for card
     byte cardchecksum;
 
-    //seek card command
+    Serial.println("Sending read request.");
     for (int i =0; i< 5; i++){
-    rfserial.write(SeekCard[i]);
+        rfserial.write(SeekCard[i]);
     }
-
+    rfserial.flush();
+    delay(1000);
+    //seek card command
     if(rfserial.available()>0)
     {
-    //check for response from module
-
-      Serial.print("n Preparing to read...n Checking Checksum n");
-      for (int i =0; i< 7; i++){
-      ModResponse[i] = rfserial.read();      
-      Serial.print(ModResponse[i], HEX);
-      Serial.print(" ");
-    }
-      Serial.println("");
-
-  //adds all the RFID command acknowledgement bytes except header and checksum
-      for (int i= 1; i<5; i++){
-     response_checksum_math += ModResponse[i];
-      }
-
+        //check for response from module
+        Serial.print("\nPreparing to read...\nChecking Checksum \n");
+        for (int i =0; i< 7; i++){
+            ModResponse[i] = rfserial.read();      
+            Serial.print(ModResponse[i], HEX);
+            Serial.print(" ");
+        }
+        Serial.println("");
+        //adds all the RFID command acknowledgement bytes except header and checksum
+        for (int i= 1; i<5; i++){
+            response_checksum_math += ModResponse[i];
+        }
         //if the response is of checksum is equal to 0xD0
         if (modrespchecksum == response_checksum_math)
         {    //then seek for card
@@ -66,40 +62,36 @@ void SeekTag()
  //wait for signal from rfid    
 
             //flushng of buffer and delay is needed to read card
-            rfserial.flush();
-            delay(5000);
+            delay(4000);
 
               //read card data, capture data and print
              if(rfserial.available())
                 {
-                    for (int i =0; i< 11; i++){
+                    int i=0;
+                    while (rfserial.available()){
                     CardResponse[i] = rfserial.read();
                     Serial.print(CardResponse[i], HEX);
-                    Serial.print(" ");  
+                    Serial.print(" ");
+                    i++; 
                     }
                     Serial.println("");
                     //check the checksum of the card
                     for (int i =1; i<9; i++){
                      cardchecksum += CardResponse[i];
                     }
-
-                    //check the checksum of the card
+                    
                     if (cardchecksum == Prim_Card[9]){
-
-                      //if the card matches then this is the primary card
-                      Serial.println("This is Primary Card");
-
+                     Serial.println("This is Primary Card");
                     }
                     else{
-                      //else this is the secondary card
                       Serial.println("Not Primary");
                     }
                 }
-
+            rfserial.flush();
         }
         else{
-        printbad();
-
+          rfserial.flush();
+          printbad();
         }
     }
 }
